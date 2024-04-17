d4af4fb3b898bddcf983bd3ebee959ec2575b2a5
Taylor Todd <64833334+Pwnyboy1031@users.noreply.github.com>
2024-04-16 22:23:15 -0500

    Cleaned up main for initialization

--- main.py
+++ main.py
@@ -14,33 +14,15 @@ import random
 # shovel_cards = [Shovel() for _ in range(10)]
 # pickaxe_cards = [Pickaxe() for _ in range(5)]
 
-#initialize players
-player1 = Player("Taylor")
-player2 = Player("Ryan")
-
-#initialize Deck
-deck = Deck()
-
-#shuffle deck
-random.shuffle(deck.cards)
-
-
-
-## Define players
-players = [player1, player2]
-
-
-# intialize cards
-game_state = GameState(players, deck)
 
 
 
         
 def main():
-    game_logic.initialize_game()
-    print(game_state.players, game_state.deck.cards)
-    player1.display_hand()
-    player2.display_hand()
+    game_state = game_logic.initialize_game()
+    #print(game_state.players, game_state.deck.cards)
+    game_state.players[0].display_hand()
+    game_state.players[1].display_hand()
 
 
 
