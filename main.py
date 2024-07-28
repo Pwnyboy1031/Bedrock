51142eb1993102ab0aa3f247306165551e474eed
Taylor Todd <64833334+Pwnyboy1031@users.noreply.github.com>
2024-07-28 18:29:59 -0500

    cleaned up main

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from card import Card
-from deck import Deck
-from game_state import GameState
-from game_logic import draw_card, play_cards, take_turn
-from shovel_card import Shovel
-from pickaxe_card import Pickaxe
-from player import Player
 import game_logic
-from spelunk_card import Spelunk
-from survey_card import Survey
-from treasure_cards import Ruby
-from menu import Menu
-import keyboard
+from game_logic import take_turn
+
 
         
 def main():
